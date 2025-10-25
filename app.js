280303da1114868b5e908622d8a4de595b97ddaf
Tanjujuba <danielsoares13.2002@gmail.com>
2025-10-25 20:12:07 -0300

    atualização botoes

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let botaoPrece = document.getElementById('conteudo__ancoras__botoes')
 let imgGaya = document.getElementById('conteudo__img')
 
@@ -8,16 +7,5 @@ botaoPrece.addEventListener('click', () => {
     setTimeout(() => {
     imgGaya.src = 'gaya.png';
     }, 3000);
-    
-=======
-let botaoPrece = document.getElementById('conteudo__ancoras__botoes')
-let imgGaya = document.getElementById('conteudo__img')
 
-botaoPrece.addEventListener('click', () => {
-    imgGaya.src = 'gaya-eating.gif';
-
-    setTimeout(() => {
-    imgGaya.src = 'gaya.png';
-    }, 3000);
->>>>>>> 089076f12d58274d0085fbb90dc88f79ed8c2cf5
-});
+} );
