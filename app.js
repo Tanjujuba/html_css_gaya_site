99cf8610d3c9b98d2422f987c18a00150116e057
Tanjujuba <danielsoares13.2002@gmail.com>
2026-04-01 20:11:36 -0300

    responsividade mobile adicionais no CSS

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-let botaoPrece = document.getElementById('conteudo__ancoras__botoes')
+let botaoPrece = document.querySelector('.conteudo__ancoras__botoes')
 let imgGaya = document.getElementById('conteudo__img')
 
 botaoPrece.addEventListener('click', () => {
-    imgGaya.src = './img/gaya-eating.gif';
 
-    setTimeout(() => {
-    imgGaya.src = './img/gaya.png';
-    alert('Hmmm! Arigatowwwww!');
-    }, 3000);
+    if(imgGaya.src == './img/gaya.png') {
+
+        setTimeout(() => {
+        imgGaya.src = './img/gaya.png';
+        alert('Hmmm! Arigatowwwww!');
+        }, 3000);
+
+    } else {
+
+        imgGaya.src = './img/gaya-fighting.gif';
+        setTimeout(() => {
+        imgGaya.src = './img/gaya-angry.png';
+        alert('Dame un Grrr!');
+    }, 8000); 
+
+    }  
 
 } );
